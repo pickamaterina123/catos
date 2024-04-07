ccebf5e8f51426a9eff593296e540a916cfebd24
grisha <grigorije05@gmail.com>
2024-04-07 17:04:38 +0200

    gif idk

--- src/lib/Gallery.svelte
+++ src/lib/Gallery.svelte
@@ -4,7 +4,7 @@
     import { v4 as uuidv4 } from 'uuid';
     import { flip } from 'svelte/animate';
 
-    const API = 'https://cataas.com/cat';
+    const API = 'https://cataas.com/cat/gif';
 
     let images = [];
     export let loading;
